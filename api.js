4f52c9ff0d1ddc66272323db02c1c053b28a426e
BISWAJIT MANDAL <mandalbis1729@gmail.com>
2021-07-21 14:34:20 +0530

    provider control enable

--- api.js
+++ api.js
@@ -3,23 +3,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-TorrentSearchApi.enableProvider('1337x');
-TorrentSearchApi.enableProvider('ThePirateBay');
+ TorrentSearchApi.enableProvider('ThePirateBay');
  //TorrentSearchApi.enableProvider('Torrent9');
  //TorrentSearchApi.enableProvider('Torrentz2');
  //TorrentSearchApi.enableProvider('KickassTorrents');
- //TorrentSearchApi.enableProvider('Rarbg');
-// TorrentSearchApi.enableProvider('TorrentProject');
+ TorrentSearchApi.enableProvider('Rarbg');
+ TorrentSearchApi.enableProvider('TorrentProject');
  TorrentSearchApi.enableProvider('Yts');
-// TorrentSearchApi.enableProvider('Limetorrents');
-//TorrentSearchApi.enableProvider('Eztv');
+ TorrentSearchApi.enableProvider('Limetorrents');
+TorrentSearchApi.enableProvider('Eztv');
+TorrentSearchApi.enableProvider('1337x');
 
-
-let torrsearch = async function(q,l) {
+let torrsearch = async function(q,l,p) {
+    
 
 // Search '1080' in 'Movies' category and limit to 20 results
 try {
-    const torrents = await TorrentSearchApi.search(q, "All", l);
+    const torrents = await TorrentSearchApi.search(p,q, "All", l);
     return torrents;
 } catch (error) {
     return error;
@@ -33,9 +33,11 @@ app.get('/:l', async function(req, res){
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,   Accept, x-client-key, x-client-token, x-client-secret, Authorization");
 
+    var p = JSON.parse(req.query.array);
     var q = req.query.q;
     var l =req.params.l
-    var value = await torrsearch(q,l).catch(error => {throw error}); 
+    
+    var value = await torrsearch(q,l,p).catch(error => {throw error}); 
     
     res.send(value);
     
