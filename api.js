ee1fb30681c712ef46788d3760327712bb9ac324
BISWAJIT MANDAL <mandalbis1729@gmail.com>
2021-07-29 14:41:39 +0530

    single mangnet link optimized

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const TorrentSearchApi = require('torrent-search-api');
 const express = require('express');
+const { json } = require('express');
 const app = express();
 const port = 3000;
 process.on('unhandledRejection', (err, p) => {
@@ -33,6 +34,39 @@ try {
 
 
 }
+let magnetSearch = async function(p) {
+    // Search '1080' in 'Movies' category and limit to 20 results
+    try {
+        //const torrents = await TorrentSearchApi.search(p,q, "All", l).catch(error => {throw error}); 
+       // console.log(torrents);
+        const m = await TorrentSearchApi.getMagnet(p);
+        //await TorrentSearchApi.downloadTorrent(torrents[l-1], '/');
+        return m;
+    } catch (error) {
+        return error;
+    }
+}
+   //magnet 
+app.get('/magnetLink', async function(req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,   Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+
+    if (req.query.p === undefined) {
+        console.log("Did not find any matching URLs")
+        return;
+     }else{
+       
+       const p = JSON.parse(
+        JSON.stringify(req.query.p)); 
+       
+       var m = await magnetSearch(p) 
+
+     }
+    //console.log(m);
+    res.send(m);
+    
+});
 
 app.get('/providers', function(req, res){
         res.header("Access-Control-Allow-Origin", "*");
@@ -69,39 +103,7 @@ app.get('/:l', async function(req, res){
 });
 
 
-let magnetSearch = async function(q,l,p) {
-    // Search '1080' in 'Movies' category and limit to 20 results
-    try {
-        const torrents = await TorrentSearchApi.search(p,q, "All", l).catch(error => {throw error}); 
-       // console.log(torrents);
-        const m = await TorrentSearchApi.getMagnet(torrents[l-1]).catch(error => {throw error}); 
-        return m;
-    } catch (error) {
-        return error;
-    }
-}
-   //magnet 
-app.get('/magnetLink/:l', async function(req, res){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,   Accept, x-client-key, x-client-token, x-client-secret, Authorization");
 
-    if (req.query.p === undefined) {
-        //console.log("Did not find any matching URLs")
-        return;
-     }else{
-        const urlp = req.query.p.replace(" ", "");
-        const names = decodeURIComponent(urlp.slice(1,urlp.length-1));
-        const p = names.split(',');
-        var q = req.query.q;
-        var l =req.params.l
-        var m = await magnetSearch(q,l,p).catch(error => {throw error}); 
-
-     }
-    //console.log(m);
-    res.send(m);
-    
-});
 
 
 
